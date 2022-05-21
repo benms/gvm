0fed7249f6139db670dd31efd0327d7ea968b8de
Dmytro Ben <dmitriy.ben@gmail.com>
2022-05-22 00:37:35 +0300

    fix: added supports for gvmrc files

--- gvm.sh
+++ gvm.sh
@@ -1,46 +1,55 @@
 # add to .zshrc or .zshrc
 # export GVM_DIR="$HOME/.gvm"
 # [ -s "$GVM_DIR/gvm.sh" ] && \. "$GVM_DIR/gvm.sh"  # This loads gvm
-
 # create dir - mkdir -p ~/.gvm
 # create dir - chmod +x ~/.gvm/gvm.sh
 
 
-# export GVM_DIR=~/.gvm
-export GVM_VER_DEFAULT=1.17.10
-export GO_VER=${GO_VER:-$GVM_VER_DEFAULT}
-export GVM_VERS_DIR=$GVM_DIR/versions
-export GVM_VENDORS_DIR_NAME=vendors
-if ! [[ -z "$GO_VER" ]]; then
-  export GOROOT=$GVM_VERS_DIR/$GO_VER/go
-  export GOPATH=$GVM_VERS_DIR/$GO_VER/$GVM_VENDORS_DIR_NAME
-  export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-fi
-export GO_DOWNLOAD_URL=https://go.dev/dl/
-
 __gvm_install() {
   local FILE=go$1.linux-amd64.tar.gz
   PATH_DIR_INST=$GVM_VERS_DIR/$1
-  PATH_ARCH=$GVM_VERS_DIR/$FILE
-  if ! [[ -f "$PATH_ARCH" ]];then
+  PATH_ARCH=$GVM_DIR/$FILE
+  if [[ ! -f "$PATH_ARCH" ]];then
     URL=$GO_DOWNLOAD_URL$FILE
     curl -L --output $PATH_ARCH $URL
   fi
   mkdir -p $PATH_DIR_INST
   tar -C $PATH_DIR_INST -xvf $PATH_ARCH
-  mkdir -p $PATH_DIR_INST/$1/$GVM_VENDORS_DIR_NAME
+  mkdir -p $PATH_DIR_INST/$GVM_VENDORS_DIR_NAME
   rm $PATH_ARCH
   echo "Successfully installed Go version $1"
 }
 
+__gvm_install_files() {
+  mkdir -p $GVM_DIR/versions
+  touch $GVM_DIR/$GVM_RC_FILE
+}
+
+__gvm_get_dir_version() {
+  head -n1 $1/$GVM_RC_FILE
+}
+
+__gvm_get_root_version() {
+  __gvm_get_dir_version $GVM_DIR
+}
+
 __gvm_use() {
-  if [[ "$1" == "${__gvm_get_current_version}" ]]; then
+  if [[ -z "$1" ]]; then
+    export GO_VER="$(__gvm_get_dir_version $PWD)"
+  elif [[ "$1" == "$(__gvm_get_current_version)" ]]; then
     echo "Go version is already $1"
     return 1
+  else
+    export GO_VER=$1
   fi
-  export GO_VER=$1
   __gvm_reload_shell
   go version && echo "Applied Go version" || echo "Not applied Go version"
+}
+
+__gvm_default() {
+  __gvm_use $1
+  echo "$1" > $GVM_DIR/$GVM_RC_FILE
+  echo "Set default version to $1"
 }
 
 __gvm_reload_shell() {
@@ -57,16 +66,15 @@ __gvm_ls_remote() {
 
 __gvm_rm() {
   local VERS="$1"
-  # local VER="${__gvm_get_current_version}"
-  # echo "Version - ${__gvm_get_current_version} - $VER - $GO_VER"
-  if [[ -z "$VERS" ]]; then
-    VERS="$GO_VER"
-  fi
-  if [[ "$VERS" = "${__gvm_get_current_version}" ]]; then
+  if [[ "$VERS" = "$(__gvm_get_current_version)" ]]; then
     echo "You deleted current version, now you should switch to another version"
   fi
   echo "Remove dir - $GVM_VERS_DIR/$VERS"
   rm -rf $GVM_VERS_DIR/$VERS/ 2>/dev/null || echo "Golang version not found"
+}
+
+__gvm_has_cmd() {
+  type "${1-}" >/dev/null 2>&1
 }
 
 __gvm_get_shell_name() {
@@ -74,21 +82,22 @@ __gvm_get_shell_name() {
 }
 
 __gvm_get_current_version() {
-  return "$(go version | awk '{print substr($3,3)}')"
+  go version | awk '{print substr($3,3)}'
 }
 
 __gvm_info() {
-  env | grep 'GO\|GVM'
+  env | grep --color=never 'GO\|GVM'
   go version 2>/dev/null || echo "Golang is not installed"
 }
 
 gvm() {
- local all_commands="use ls ls-remote rm info install"
- local commands_with_versions="use rm install"
+ local all_commands="use ls ls-remote rm info install default"
+ local commands_with_versions="default rm install"
 
  if [[ -z "$1" ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
   echo "GVM help:"
   echo " - use <go-version> - use the version"
+  echo " - default <go-version> - set default the version"
   echo " - info - show info about current version"
   echo " - ls - list available installed versions that you can switch through the command use"
   echo " - ls-remote - list available versions for installation"
@@ -98,10 +107,22 @@ gvm() {
  elif [[ $commands_with_versions =~ (^|[[:space:]])$1($|[[:space:]]) ]] && [[ -z "$2" ]]; then
   echo "You should select Golang"
   return 1
-    elif [[ $all_commands =~ (^|[[:space:]])$1($|[[:space:]]) ]]; then
+ elif [[ $all_commands =~ (^|[[:space:]])$1($|[[:space:]]) ]]; then
   sub_cmd=$(echo "$1" | sed -r 's/[-]+/_/g')
   __gvm_$sub_cmd $2
  else
   echo "Command not found"
  fi
 }
+
+# export GVM_DIR=~/.gvm
+export GVM_RC_FILE=.gvmrc
+export GO_VER=${GO_VER:-$(__gvm_get_root_version)}
+export GVM_VERS_DIR=$GVM_DIR/versions
+export GVM_VENDORS_DIR_NAME=vendors
+if ! [[ -z "$GO_VER" ]]; then
+  export GOROOT=$GVM_VERS_DIR/$GO_VER/go
+  export GOPATH=$GVM_VERS_DIR/$GO_VER/$GVM_VENDORS_DIR_NAME
+  export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+fi
+export GO_DOWNLOAD_URL=https://go.dev/dl/
