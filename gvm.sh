257a5f1e7f1ee04093cfc3286a53d9db29f8784e
Dmytro Ben <dmitriy.ben@gmail.com>
2022-05-21 12:05:38 +0300

    fix(sh): grooming

--- gvm.sh
+++ gvm.sh
@@ -1,17 +1,17 @@
-export GOMY=$HOME/go/my
-export GOVERDEFAULT=1.17.10
-export GOVER=${GOVER:-$GOVERDEFAULT}
-export GOROOT=$GOMY/$GOVER/go
 export GOPATH=$HOME/go
+export GVM_DIR=$GOPATH/my
+export GVM_VER_DEFAULT=1.17.10
+export GO_VER=${GO_VER:-$GVM_VER_DEFAULT}
+export GOROOT=$GVM_DIR/$GO_VER/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+export GO_DOWNLOAD_URL=https://go.dev/dl/
 
-go_fetch() {
+gvm_install() {
   FILE=go$1.linux-amd64.tar.gz
-  PATH_DIR_INST=$GOMY/$1
-  PATH_ARCH=$GOMY/$FILE
+  PATH_DIR_INST=$GVM_DIR/$1
+  PATH_ARCH=$GVM_DIR/$FILE
   if ! [[ -f "$PATH_ARCH" ]];then
-    URL=https://go.dev/dl/$FILE
-    #wget -O $PATH_ARCH $URL
+    URL=$GO_DOWNLOAD_URL$FILE
     curl -L --output $PATH_ARCH $URL
   fi
   mkdir -p $PATH_DIR_INST
@@ -20,26 +20,43 @@ go_fetch() {
   echo "Successfully fetched Go version $1"
 }
 
-go_use() {
-  export GOVER=$1
-  source ~/.zshrc
+gvm_use() {
+  export GO_VER=$1
+  source ~/.$(gvm_get_shell_name)rc
   go version && echo "Applied Go version" || echo "Not applied Go version"
 }
 
-go_ls() {
-  ls -1 $GOPATH/my
+gvm_ls() {
+  ls -1 $GVM_DIR
 }
 
-go_ls_remote() {
- curl -s https://go.dev/dl/| grep "class=\"download\"" | grep linux-amd64 | awk 'match($0, /go[1-9]\.[0-9a-z]+\.?[0-9]+/) { print substr( $0, RSTART, RLENGTH )}'|less
+gvm_ls_remote() {
+ curl -s $GO_DOWNLOAD_URL| grep "class=\"download\"" | grep linux-amd64 | awk 'match($0, /go[1-9]\.[0-9a-z]+\.?[0-9]+/) { print substr( $0, RSTART, RLENGTH )}'|less
 }
 
-go_rm() {
- go_use $GOVERDEFAULT
- rm -rf $GOMY/$1
+gvm_rm() {
+ gvm_use $GVM_VER_DEFAULT
+ rm -rf $GVM_DIR/$1
+}
+
+gvm_get_shell_name() {
+  echo "$SHELL" | awk '{n=split($1,A,"/"); print A[n]}'
 }
 
 gvm() {
- sub_cmd=`echo "$1" | sed -r 's/[-]+/_/g'`
- go_$sub_cmd $2
+ available_list_of_commands="use ls ls-remote rm"
+ if [[ -z "$1" ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+  echo "GVM help:"
+  echo " - use <go-version> - use the version"
+  echo " - ls - list available installed versions that you can switch through the command use"
+  echo " - ls-remote - list available versions for installation"
+  echo " - rm <go-version> - remove installed go version"
+  return 0
+ fi
+ if [[ $available_list_of_commands =~ (^|[[:space:]])$1($|[[:space:]]) ]]; then
+  sub_cmd=$(echo "$1" | sed -r 's/[-]+/_/g')
+  gvm_$sub_cmd $2
+ else
+  echo "Command not found"
+ fi
 }
