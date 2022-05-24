cb5ad20be4db6836fa15b1e50b49b7e7f97cd6fa
Dmytro Ben <dmitriy.ben@gmail.com>
2022-05-24 11:29:46 +0300

    formatted

--- gvm.sh
+++ gvm.sh
@@ -2,7 +2,7 @@ __gvm_install() {
   local FILE=go$1.linux-amd64.tar.gz
   PATH_DIR_INST=$GVM_VERS_DIR/$1
   PATH_ARCH=$GVM_DIR/$FILE
-  if [[ ! -f "$PATH_ARCH" ]];then
+  if [[ ! -f "$PATH_ARCH" ]]; then
     URL=$GO_DOWNLOAD_URL$FILE
     curl -L --output $PATH_ARCH $URL
   fi
@@ -41,7 +41,7 @@ __gvm_use() {
 
 __gvm_default() {
   __gvm_use $1
-  echo "$1" > $GVM_DIR/$GVM_RC_FILE
+  echo "$1" >$GVM_DIR/$GVM_RC_FILE
   echo "Set default version to $1"
 }
 
@@ -51,12 +51,12 @@ __gvm_reload_shell() {
 
 __gvm_ls() {
   echo "GVM Installed versions:"
-  ls -1 $GVM_VERS_DIR|sort -V -r|awk '{printf "\t"$0"\n"}'
+  ls -1 $GVM_VERS_DIR | sort -V -r | awk '{printf "\t"$0"\n"}'
 }
 
 __gvm_ls_remote() {
- echo "Loading available for installing versions..."
- curl -s $GO_DOWNLOAD_URL| grep "class=\"download\"" | grep linux-amd64 | awk 'match($0, /go[1-9]\.[0-9a-z]+\.?[0-9]+/) { print substr( $0, RSTART, RLENGTH )}'|sort -V -r|awk '{printf "\t"substr($0,3)"\n"}'|less -X
+  echo "Loading available for installing versions..."
+  curl -s $GO_DOWNLOAD_URL | grep "class=\"download\"" | grep linux-amd64 | awk 'match($0, /go[1-9]\.[0-9a-z]+\.?[0-9]+/) { print substr( $0, RSTART, RLENGTH )}' | sort -V -r | awk '{printf "\t"substr($0,3)"\n"}' | less -X
 }
 
 __gvm_rm() {
@@ -92,66 +92,67 @@ __gvm_info() {
 gvm-application() {
   local all_commands="no-preserve-uninstall"
   if [[ -z "$1" ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
-  echo "GVM application help:"
-  echo " - no-preserve-uninstall - uninstall gvm application"
-  return 0
- elif [[ $all_commands =~ (^|[[:space:]])$1($|[[:space:]]) ]]; then
-  sudo rm -rf $GVM_DIR 2>/dev/null || echo "Can't remove $GVM_DIR, probably it's already deleted"
-  local SHELL_RC_FILE=~/.$(__gvm_get_shell_name)rc
-  local begin_line=$(grep -n "#-begin-GVM-block-" $SHELL_RC_FILE| awk -F ':' '{print $1}'| head -n1)
-  local end_line=$(grep -n "#-end-GVM-block-" $SHELL_RC_FILE| awk -F ':' '{print $1}'| head -n1)
-  if [[ -z "$begin_line" ]] || [[ -z "$end_line" ]]; then
-    echo "Not found block in shell rc file"
+    echo "GVM application help:"
+    echo " - no-preserve-uninstall - uninstall gvm application"
     return 0
+  elif [[ $all_commands =~ (^|[[:space:]])$1($|[[:space:]]) ]]; then
+    sudo rm -rf $GVM_DIR 2>/dev/null || echo "Can't remove $GVM_DIR, probably it's already deleted"
+    local SHELL_RC_FILE=~/.$(__gvm_get_shell_name)rc
+    local begin_line=$(grep -n "#-begin-GVM-block-" $SHELL_RC_FILE | awk -F ':' '{print $1}' | head -n1)
+    local end_line=$(grep -n "#-end-GVM-block-" $SHELL_RC_FILE | awk -F ':' '{print $1}' | head -n1)
+    if [[ -z "$begin_line" ]] || [[ -z "$end_line" ]]; then
+      echo "Not found block in shell rc file"
+      return 0
+    fi
+    sed "${begin_line},${end_line}d" $SHELL_RC_FILE >${SHELL_RC_FILE}_new
+    mv ${SHELL_RC_FILE}_new ${SHELL_RC_FILE}
+    __gvm_reload_shell
+    echo "Application gvm successfully uninstalled"
+  else
+    echo "Command not found"
   fi
-  sed "${begin_line},${end_line}d" $SHELL_RC_FILE > ${SHELL_RC_FILE}_new; mv ${SHELL_RC_FILE}_new ${SHELL_RC_FILE}
-  __gvm_reload_shell
-  echo "Application gvm successfully uninstalled"
- else
-  echo "Command not found"
- fi
 }
 
 gvm() {
- local all_commands="use ls ls-remote rm info install default"
- local commands_with_versions="default rm install"
+  local all_commands="use ls ls-remote rm info install default"
+  local commands_with_versions="default rm install"
 
- if [[ -z "$1" ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
-  __gvm_help_title
-  echo "Golang version manager"
-  echo "\nOptions:"
-  echo "  -h, --help\t\t\tShow this message and exit"
-  echo "\nUsage Examples:"
-  echo "   Install Golang version 1.17.10"
-  echo "   $ gvm install 1.17.10"
-  echo "\n   Set and use Golang version 1.17.10 as default"
-  echo "   $ gvm default 1.17.10"
-  echo "\n   Use Golang version 1.18.2 (works only for current terminal session)"
-  echo "   $ gvm use 1.18.2"
-  echo "\n   Show installed Golang versions available for switching"
-  echo "   $ gvm ls"
-  echo "\n   Show all versions that can be installed from go.dev repo"
-  echo "   $ gvm ls-remote"
-  echo "\n   Remove installed version"
-  echo "   $ gvm rm 1.18.2"
-  echo "\nCommands:"
-  echo "  use <version>\t\tuse the version"
-  echo "  default <version>\t\tset default the version"
-  echo "  info\t\t\t\tshow info about current version"
-  echo "  ls\t\t\t\tlist available installed versions that you can switch through the command use"
-  echo "  ls-remote\t\t\tlist available versions for installation"
-  echo "  install <version>\t\tinstall go version"
-  echo "  rm <version>\t\tremove installed go version"
-  return 0
- elif [[ $commands_with_versions =~ (^|[[:space:]])$1($|[[:space:]]) ]] && [[ -z "$2" ]]; then
-  echo "You should select Golang"
-  return 1
- elif [[ $all_commands =~ (^|[[:space:]])$1($|[[:space:]]) ]]; then
-  sub_cmd=$(echo "$1" | sed -r 's/[-]+/_/g')
-  __gvm_$sub_cmd $2
- else
-  __gvm_no_such_command $1
- fi
+  if [[ -z "$1" ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+    __gvm_help_title
+    echo "Golang version manager"
+    echo "\nOptions:"
+    echo "  -h, --help\t\t\tShow this message and exit"
+    echo "\nUsage Examples:"
+    echo "   Install Golang version 1.17.10"
+    echo "   $ gvm install 1.17.10"
+    echo "\n   Set and use Golang version 1.17.10 as default"
+    echo "   $ gvm default 1.17.10"
+    echo "\n   Use Golang version 1.18.2 (works only for current terminal session)"
+    echo "   $ gvm use 1.18.2"
+    echo "\n   Show installed Golang versions available for switching"
+    echo "   $ gvm ls"
+    echo "\n   Show all versions that can be installed from go.dev repo"
+    echo "   $ gvm ls-remote"
+    echo "\n   Remove installed version"
+    echo "   $ gvm rm 1.18.2"
+    echo "\nCommands:"
+    echo "  use <version>\t\tuse the version"
+    echo "  default <version>\t\tset default the version"
+    echo "  info\t\t\t\tshow info about current version"
+    echo "  ls\t\t\t\tlist available installed versions that you can switch through the command use"
+    echo "  ls-remote\t\t\tlist available versions for installation"
+    echo "  install <version>\t\tinstall go version"
+    echo "  rm <version>\t\tremove installed go version"
+    return 0
+  elif [[ $commands_with_versions =~ (^|[[:space:]])$1($|[[:space:]]) ]] && [[ -z "$2" ]]; then
+    echo "You should select Golang"
+    return 1
+  elif [[ $all_commands =~ (^|[[:space:]])$1($|[[:space:]]) ]]; then
+    sub_cmd=$(echo "$1" | sed -r 's/[-]+/_/g')
+    __gvm_$sub_cmd $2
+  else
+    __gvm_no_such_command $1
+  fi
 }
 
 __gvm_help_title() {
