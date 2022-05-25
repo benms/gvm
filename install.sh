48bbc2765dfb514c6b1cfd6a358a24613a209dbe
Dmytro Ben <dmitriy.ben@gmail.com>
2022-05-25 06:32:06 +0300

    fix wf

--- install.sh
+++ install.sh
@@ -6,25 +6,25 @@ __gvm_get_shell_name() {
 }
 
 __gvm_reload_shell() {
-  # shellcheck source=/dev/null
   source "$HOME/.$(__gvm_get_shell_name)rc"
 }
 
 export RC_FILE
 RC_FILE="$HOME/.$(__gvm_get_shell_name)rc"
-export GVM_DIR=$HOME/.gvm
+export GVM_DIR="$HOME/.gvm"
 export SHELL_URL=https://raw.githubusercontent.com/benms/gvm/main/gvm.sh
 
 {
-  echo "#-begin-GVM-block-"
-  echo "export GVM_DIR=\"\$HOME/.gvm\""
-  echo "[ -s \"\$GVM_DIR/gvm.sh\" ] && \. \"\$GVM_DIR/gvm.sh\""
-  echo "#-end-GVM-block-"
-} >> "$RC_FILE"
+  printf "#-begin-GVM-block-\n"
+  printf "export GVM_DIR=\"\$HOME/.gvm\"\n"
+  printf "[ -s \"\$GVM_DIR/gvm.sh\" ] && \. \"\$GVM_DIR/gvm.sh\"\n"
+  printf "#-end-GVM-block-\n"
+} >> $RC_FILE
 
 mkdir -p "$GVM_DIR"
 curl -L --output "$GVM_DIR/gvm.sh" "$SHELL_URL"
 chmod +x "$GVM_DIR/gvm.sh"
 mkdir -p "$GVM_DIR/versions"
 touch "$GVM_DIR/.gvmrc"
-__gvm_reload_shell
+
+echo "GVM Installation finished"
